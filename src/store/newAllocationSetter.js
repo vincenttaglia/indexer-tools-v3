04fb8139db8dacef0578a4689d90e81dd2a2e327
Vincent Taglia <info@vincenttaglia.com>
2023-03-17 14:41:40 -0500

    Fix for slider

--- src/store/newAllocationSetter.js
+++ src/store/newAllocationSetter.js
@@ -46,7 +46,7 @@ export const useNewAllocationSetterStore = defineStore('allocationSetter', {
       let dailyRewards = [];
       for(let i = 0; i < subgraphStore.getSelectedSubgraphs.length; i++){
         let subgraph = subgraphStore.getSelectedSubgraphs[i];
-        if(subgraph.currentSignalledTokens > 0) {
+        if(subgraph.currentVersion.subgraphDeployment.stakedTokens > 0 && !accountStore.loading) {
           dailyRewards[i] = { dailyRewards: calculateSubgraphDailyRewards(subgraph.currentSignalledTokens, subgraph.currentVersion.subgraphDeployment.stakedTokens, networkStore, state.newAllocations[subgraph.id]) }
         }else{
           dailyRewards[i] = { dailyRewards: 0 }
