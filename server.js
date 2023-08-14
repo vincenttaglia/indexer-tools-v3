145e23f6a7b0c9a984155413061f3cf85da1a729
Vincent Taglia <info@vincenttaglia.com>
2023-08-14 18:29:45 -0500

    Fix for non-root paths GET request

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+var history = require('connect-history-api-fallback');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
+app.use(history());
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
