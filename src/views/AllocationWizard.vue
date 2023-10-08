934b1f5edd238680e7f2b801593b3bc41f5d023c
Vincent Taglia <info@vincenttaglia.com>
2023-10-08 00:10:10 -0500

    Send actions to agent

--- src/views/AllocationWizard.vue
+++ src/views/AllocationWizard.vue
@@ -18,6 +18,13 @@
       </template>
       <template v-slot:item.4>
         <div class="mt-12 mb-10 ml-5">
+          <v-btn
+            @click="sendActionsToAgent()"
+          >
+            Queue actions with Indexer Agent
+          </v-btn>
+        </div>
+        <div class="mt-12 mb-10 ml-5">
           <h3>Action Queue Commands <small>(>=v0.20.0)</small></h3>
           <v-textarea readonly :value="newAllocationSetterStore.actionsQueueBuildCommands" rows="10" ></v-textarea>
           <h3>Indexing Rule Commands</h3>
@@ -88,11 +95,94 @@ import { useAllocationStore } from "@/store/allocations";
 import { useNetworkStore } from "@/store/network";
 import { useNewAllocationSetterStore } from "@/store/newAllocationSetter";
 import { VStepper } from 'vuetify/labs/VStepper'
+import { agentApolloClient } from "@/plugins/graphAgentClient";
+import gql from "graphql-tag";
 
 const allocationStore = useAllocationStore();
 const networkStore = useNetworkStore();
 const newAllocationSetterStore = useNewAllocationSetterStore();
 
+function sendActionsToAgent(){
+  agentApolloClient.mutate({
+    mutation: gql`mutation queueActions($actions: [ActionInput!]!){
+      queueActions(actions: $actions) {
+        actions{
+          id
+          status
+          type
+          deploymentID
+          allocationID
+          amount
+          poi
+          force
+          priority
+          source
+          reason
+          transaction
+          failureReason
+          createdAt
+          updatedAt
+          protocolNetwork
+        }
+      }
+    }`,
+    variables: { actions: newAllocationSetterStore.actionsQueueBuildAPIObject }
+  }).then((data) => {
+    console.log("AGENT CONNECT SEND ACTIONS DATA");
+    console.log(data);
+  });
+}
+function allocate(deployment, amount, protocolNetwork){
+  return agentApolloClient.mutate({
+    mutation: gql`mutation createAllocation($deployment: String!, $amount: String!, $protocolNetwork: String!){
+      createAllocation(deployment: $deployment, amount: $amount, protocolNetwork: $protocolNetwork) {
+        allocation
+        deployment
+        allocatedTokens
+        protocolNetwork
+      }
+    }`,
+    variables: { deployment: deployment, amount: amount, protocolNetwork: protocolNetwork }
+  }).then((data) => {
+    console.log("AGENT CONNECT ALLOCATE DATA");
+    console.log(data);
+  });
+}
+function unallocate(allocation, protocolNetwork){
+  return agentApolloClient.mutate({
+    mutation: gql`mutation closeAllocation($allocation: String!, $protocolNetwork: String!){
+      closeAllocation(allocation: $allocation, protocolNetwork: $protocolNetwork) {
+        allocation
+        allocatedTokens
+        indexingRewards
+        receiptsWorthCollecting
+        protocolNetwork
+      }
+    }`,
+    variables: { allocation: allocation, protocolNetwork: protocolNetwork }
+  }).then((data) => {
+    console.log("AGENT CONNECT UNALLOCATE DATA");
+    console.log(data);
+  });
+}
+function reallocate(allocation, amount, protocolNetwork){
+  return agentApolloClient.mutate({
+    mutation: gql`mutation reallocateAllocation($allocation: String!, $amount: String!, $protocolNetwork: String!){
+      createAllocation(allocation: $allocation, amount: $amount, protocolNetwork: $protocolNetwork) {
+        closedAllocation
+        indexingRewardsCollected
+        receiptsWorthCollecting
+        createdAllocation
+        createdAllocationStake
+        protocolNetwork
+      }
+    }`,
+    variables: { allocation: allocation, amount: amount, protocolNetwork: protocolNetwork }
+  }).then((data) => {
+    console.log("AGENT CONNECT REALLOCATE DATA");
+    console.log(data);
+  });
+}
 
 </script>
 
