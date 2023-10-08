9799e44d06276174e1c83bb2cfd0065eeb6ae62d
Vincent Taglia <info@vincenttaglia.com>
2023-10-08 15:43:30 -0500

    CORS

--- src/store/accounts.js
+++ src/store/accounts.js
@@ -29,8 +29,7 @@ export const useAccountStore = defineStore('accountStore', {
       const httpLink = createHttpLink({
         uri: state.getAgentConnectEndpoint,
         headers: {
-          "Access-Control-Request-Private-Network": true,
-          "Access-Control-Allow-Origin": '*',
+          "Access-Control-Request-Private-Network": "true",
         },
         /*fetchOptions: {
           mode: 'no-cors',
