2667087ce4a8bf8db5ac0dc91434c7a47bbd4d8f
Vincent Taglia <info@vincenttaglia.com>
2023-10-22 13:58:58 -0500

    Better nav experience for smaller screens

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -5,7 +5,7 @@
   color="#5a3c57"
   height=75
   >
-    
+    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     <div 
     style="
       display:flex;
@@ -19,7 +19,7 @@
       line-height: 1.75rem;
       text-transform: none;
       ">
-      <div>
+      <div style="white-space: nowrap;">
         <v-icon icon="mdi-tools" />
 
         Indexer Tools v3
@@ -27,6 +27,7 @@
       <v-select
         label="Chain"
         class="ml-5 mr-2 flex-left chain"
+        style="min-width: 150px"
         :items="chainStore.getChains.map(chain => chain.id)"
         :value="chainStore.getChainID"
         @update:model-value="updateChain"
@@ -34,44 +35,58 @@
         
       ></v-select>
     </div>
-    <v-btn
-        variant="text"
-        to="/"
-        class="ml-5 mr-2"
-        exact
-    >
-      Subgraphs Dashboard
-    </v-btn>
-    <v-btn
-        variant="text"
-        to="/allocations"
-        class="ml-2 mr-2"
-    >
-      Allocations Dashboard
-    </v-btn>
-    <v-btn
-        variant="text"
-        to="/wizard"
-        class="ml-2 mr-2"
-    >
-      Allocation Wizard
-    </v-btn>
-    <v-btn
-        variant="text"
-        to="/actions-manager"
-        class="ml-2 mr-2"
-    >
-      Actions Manager
-    </v-btn>
-    <v-btn
-        variant="text"
-        to="/settings"
-        class="ml-2 mr-2"
-    >
-      Settings
-    </v-btn>
+    <div style="overflow: hidden;white-space: nowrap;">
+      <v-btn
+          variant="text"
+          to="/"
+          class="ml-5 mr-2"
+          exact
+      >
+        Subgraphs Dashboard
+      </v-btn>
+      <v-btn
+          variant="text"
+          to="/allocations"
+          class="ml-2 mr-2"
+      >
+        Allocations Dashboard
+      </v-btn>
+      <v-btn
+          variant="text"
+          to="/wizard"
+          class="ml-2 mr-2"
+      >
+        Allocation Wizard
+      </v-btn>
+      <v-btn
+          variant="text"
+          to="/actions-manager"
+          class="ml-2 mr-2"
+      >
+        Actions Manager
+      </v-btn>
+      <v-btn
+          variant="text"
+          to="/settings"
+          class="ml-2 mr-2"
+      >
+        Settings
+      </v-btn>
+    </div>
     <accounts-dropdown></accounts-dropdown>
   </v-app-bar>
+  <v-navigation-drawer
+    v-model="drawer"
+    temporary
+  >
+    <v-list nav>
+      <v-list-item title="Subgraphs Dashboard" to="/" exact></v-list-item>
+      <v-list-item title="Allocations Dashboard" to="/allocations"></v-list-item>
+      <v-list-item title="Allocation Wizard" to="/wizard"></v-list-item>
+      <v-list-item title="Actions Manager" to="/actions-manager"></v-list-item>
+      <v-list-item title="Settings" to="/settings"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script setup>
@@ -80,10 +95,12 @@
   import { useSubgraphsStore } from '@/store/subgraphs';
   import { useAllocationStore } from '@/store/allocations';
   import { useChainStore } from '@/store/chains';
+  import { ref } from 'vue';
   const chainStore = useChainStore();
   const networkStore = useNetworkStore();
   const subgraphStore = useSubgraphsStore();
   const allocationStore = useAllocationStore();
+  const drawer = ref(false);
 
   function updateChain(id){
     chainStore.setChain(id);
