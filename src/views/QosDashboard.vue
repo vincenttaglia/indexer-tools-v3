217e88ce577cbe3ff6157e39ed0d9e39eccf1063
Vincent Taglia <info@vincenttaglia.com>
2024-10-11 00:10:55 -0500

    QOS Dashboard formatting

--- src/views/QosDashboard.vue
+++ src/views/QosDashboard.vue
@@ -6,6 +6,7 @@
       loading-text="Loading... Please wait"
       mobile-breakpoint="0"
       hover
+      items-per-page="25"
   >
     <template v-slot:no-data>
       <p class="mt-4">
@@ -21,6 +22,21 @@
         Reset Filters
       </v-btn>
     </template>
+    <template v-slot:item.query_count="{ item }">
+      {{ numeral(item.query_count).format('0,0') }}
+    </template>
+    <template v-slot:item.total_query_fees="{ item }">
+      {{ numeral(item.total_query_fees).format('0,0') }} GRT
+    </template>
+    <template v-slot:item.avg_gateway_latency_ms="{ item }">
+      {{ numeral(item.avg_gateway_latency_ms).format('0,0.00') }} ms
+    </template>
+    <template v-slot:item.avg_query_fee="{ item }">
+      {{ numeral(item.avg_query_fee).format('0,0.00000') }} GRT
+    </template>
+    <template v-slot:item.gateway_query_success_rate="{ item }">
+      {{ numeral(item.gateway_query_success_rate).format('0.00%') }}
+    </template>
   </v-data-table>
 </template>
 
@@ -30,6 +46,7 @@ import gql from "graphql-tag";
 import { useAccountStore } from "@/store/accounts";
 import { storeToRefs } from "pinia";
 import { qosSubgraphClient } from "@/plugins/qosSubgraphClient";
+import numeral from "numeral";
 
 const accountStore = useAccountStore();
 //const { getActiveAccount } = storeToRefs(accountStore);
@@ -82,7 +99,7 @@ function resetFilters () {
 const headers = ref([
   { title: 'ID', key: 'subgraphDeployment.id' },
   //{ title: 'Chain ID', key: 'chain_id'},
-  { title: 'Query Count', key: 'query_count' },
+  { title: 'Query Count (1d)', key: 'query_count' },
   { title: 'Query Fees (1d)', key: 'total_query_fees'},
   { title: 'Avg Latency', key: 'avg_gateway_latency_ms' },
   { title: 'Avg Query Fee', key: 'avg_query_fee'},
