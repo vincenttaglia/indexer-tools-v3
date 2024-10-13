13a7b1e988a9ba91707f485fa31a551d8a2889ec
Vincent Taglia <info@vincenttaglia.com>
2024-10-13 11:46:04 -0500

    fix: new settings params not overridden by imported settings from cache

--- src/store/subgraphSettings.js
+++ src/store/subgraphSettings.js
@@ -1,34 +1,36 @@
 // Utilities
 import { defineStore } from 'pinia'
 
+const settingsDefault = {
+  search: '',
+  minSignal: '',
+  maxSignal: '',
+  newAllocation: '100000',
+  targetApr: '10',
+  noRewardsFilter: 0,
+  networkFilter: [],
+  activateBlacklist: false,
+  activateSynclist: false,
+  statusFilter: 'none',
+  automaticIndexingRewards: false,
+  subgraphBlacklist: "",
+  subgraphSynclist: "",
+  rpc: {
+    mainnet: "",
+    arbitrum: "",
+    sepolia: "",
+    arbitrumSepolia: "",
+  },
+  hideCurrentlyAllocated: false,
+  queryFilters: {
+    minSignal: '',
+    networkFilter: [],
+  }
+};
+
 export const useSubgraphSettingStore = defineStore('subgraphSetting', {
   state: () => ({
-    settings: localStorage.subgraphSettings ? JSON.parse(localStorage.subgraphSettings) : {
-      search: '',
-      minSignal: '',
-      maxSignal: '',
-      newAllocation: '100000',
-      targetApr: '10',
-      noRewardsFilter: 0,
-      networkFilter: [],
-      activateBlacklist: false,
-      activateSynclist: false,
-      statusFilter: 'none',
-      automaticIndexingRewards: false,
-      subgraphBlacklist: "",
-      subgraphSynclist: "",
-      rpc: {
-        mainnet: "",
-        arbitrum: "",
-        sepolia: "",
-        arbitrumSepolia: "",
-      },
-      hideCurrentlyAllocated: false,
-      queryFilters: {
-        minSignal: '',
-        networkFilter: [],
-      },
-    },
+    settings: localStorage.subgraphSettings ? Object.assign({}, settingsDefault, JSON.parse(localStorage.subgraphSettings)) : Object.assign({}, settingsDefault),
   }),
   getters: {
     search: (state) => state.settings.search,
