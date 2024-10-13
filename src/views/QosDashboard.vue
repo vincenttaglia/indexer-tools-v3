9fdde7bdd3260db2936319f2b022ab291753f92d
Vincent Taglia <info@vincenttaglia.com>
2024-10-13 00:20:54 -0500

    feat: query filtering for QoS Dashboard

--- src/views/QosDashboard.vue
+++ src/views/QosDashboard.vue
@@ -1,4 +1,19 @@
 <template>
+  <div class="d-flex align-content-center">
+    <v-btn text="Refresh Subgraphs" prepend-icon="mdi-refresh" @click="queryQos()" class="mx-5 my-6" stacked></v-btn>
+    <div class="d-flex align-content-center align-center justify-center">
+      <v-combobox
+          v-model="subgraphSettingStore.settings.queryFilters.networkFilter"
+          :items="networks"
+          label="Subgraph Networks"
+          multiple
+          chips
+          clearable
+          class="d-inline-block mx-4"
+          style="min-width:13rem;max-width: 15rem;top: -5px"
+      ></v-combobox>
+    </div>
+  </div>
   <v-data-table
       :headers="headers"
       :items="qosData"
@@ -7,6 +22,7 @@
       mobile-breakpoint="0"
       hover
       items-per-page="25"
+      :loading = "loading"
   >
     <template v-slot:no-data>
       <p class="mt-4">
@@ -47,50 +63,81 @@ import { useAccountStore } from "@/store/accounts";
 import { storeToRefs } from "pinia";
 import { qosSubgraphClient } from "@/plugins/qosSubgraphClient";
 import numeral from "numeral";
+import { useSubgraphSettingStore } from "@/store/subgraphSettings";
+import { networks } from "@/plugins/subgraphNetworks";
 
 const accountStore = useAccountStore();
+const subgraphSettingStore = useSubgraphSettingStore();
 //const { getActiveAccount } = storeToRefs(accountStore);
 const qosData = ref([]);
-
-console.log("QOS DATA");
-qosSubgraphClient.query({
-  query: gql`query{
-    queryDailyDataPoints(orderBy: dayNumber, first: 1, orderDirection: desc) {
-      dayNumber
-    }
-  }`,
-})
-.then(({ data }) => {
-  console.log("DAY NUMBER");
-  console.log(data);
-  return qosSubgraphClient.query({
-    query: gql`query queryDailyDataPoints($dayNumber: Int!){
-      queryDailyDataPoints(
-        orderBy: total_query_fees
-        where: {dayNumber: $dayNumber}
-        orderDirection: desc
-      ) {
-        dayNumber
-        chain_id
-        avg_query_fee
-        avg_gateway_latency_ms
-        gateway_query_success_rate
-        query_count
-        total_query_fees
-        subgraphDeployment {
-          id
+const loading = ref(true);
+const QOS_QUERY_NO_FILTER = gql`query queryDailyDataPoints($dayNumber: Int!){
+        queryDailyDataPoints(
+          orderBy: total_query_fees
+          where: {dayNumber: $dayNumber}
+          orderDirection: desc
+        ) {
+          dayNumber
+          chain_id
+          avg_query_fee
+          avg_gateway_latency_ms
+          gateway_query_success_rate
+          query_count
+          total_query_fees
+          subgraphDeployment {
+            id
+          }
         }
+      }`;
+const QOS_QUERY = gql`query queryDailyDataPoints($dayNumber: Int!, $networkFilter: [String]!){
+        queryDailyDataPoints(
+          orderBy: total_query_fees
+          where: {dayNumber: $dayNumber, chain_id_in: $networkFilter}
+          orderDirection: desc
+        ) {
+          dayNumber
+          chain_id
+          avg_query_fee
+          avg_gateway_latency_ms
+          gateway_query_success_rate
+          query_count
+          total_query_fees
+          subgraphDeployment {
+            id
+          }
+        }
+      }`;
+
+queryQos();
+
+
+function queryQos(){
+  console.log("QOS DATA");
+  loading.value = true;
+  qosSubgraphClient.query({
+    query: gql`query{
+      queryDailyDataPoints(orderBy: dayNumber, first: 1, orderDirection: desc) {
+        dayNumber
       }
     }`,
-    variables: {
-      dayNumber: data.queryDailyDataPoints[0].dayNumber - 1
-    }
-  }).then(({ data }) => {
-    console.log("QUERY DAILY DATA POINTS");
-    console.log(data.queryDailyDataPoints);
-    qosData.value = data.queryDailyDataPoints;
   })
-});
+  .then(({ data }) => {
+    console.log("DAY NUMBER");
+    console.log(data);
+    return qosSubgraphClient.query({
+      query: subgraphSettingStore.settings.queryFilters.networkFilter.length > 0 ? QOS_QUERY : QOS_QUERY_NO_FILTER,
+      variables: {
+        dayNumber: data.queryDailyDataPoints[0].dayNumber - 1,
+        networkFilter: subgraphSettingStore.settings.queryFilters.networkFilter,
+      }
+    }).then(({ data }) => {
+      console.log("QUERY DAILY DATA POINTS");
+      console.log(data.queryDailyDataPoints);
+      qosData.value = data.queryDailyDataPoints;
+      loading.value = false;
+    })
+  });
+}
 
 function resetFilters () {
 
@@ -98,7 +145,7 @@ function resetFilters () {
 
 const headers = ref([
   { title: 'ID', key: 'subgraphDeployment.id' },
-  //{ title: 'Chain ID', key: 'chain_id'},
+  { title: 'Chain ID', key: 'chain_id'},
   { title: 'Query Count (1d)', key: 'query_count' },
   { title: 'Query Fees (1d)', key: 'total_query_fees'},
   { title: 'Avg Latency', key: 'avg_gateway_latency_ms' },
