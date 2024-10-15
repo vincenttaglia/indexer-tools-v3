e036e4cb3e88c00e570cecccb84e31b15d48c6a3
Vincent Taglia <info@vincenttaglia.com>
2024-10-14 22:46:05 -0500

    feat: QoS dashboard columns and formatting

--- src/views/QosDashboard.vue
+++ src/views/QosDashboard.vue
@@ -30,20 +30,36 @@
       <StatusDropdownVue :item='item' />
     </template>
     <template v-slot:item.query_count="{ item }">
-      {{ numeral(item.query_count).format('0,0') }}
+      {{ numeral(item.query_count).format('0,0') }} queries
     </template>
     <template v-slot:item.total_query_fees="{ item }">
       {{ numeral(item.total_query_fees).format('0,0') }} GRT
     </template>
     <template v-slot:item.avg_indexer_latency_ms="{ item }">
-      {{ numeral(item.avg_indexer_latency_ms).format('0,0.00') }} ms
+      {{ numeral(item.avg_indexer_latency_ms).format('0,0.0') }} ms
+    </template>
+    <template v-slot:item.max_indexer_latency_ms="{ item }">
+      {{ numeral(item.max_indexer_latency_ms).format('0,0.0') }} ms
     </template>
     <template v-slot:item.avg_query_fee="{ item }">
       {{ numeral(item.avg_query_fee).format('0,0.00000') }} GRT
     </template>
+    <template v-slot:item.max_query_fee="{ item }">
+      {{ numeral(item.max_query_fee).format('0,0.00000') }} GRT
+    </template>
     <template v-slot:item.proportion_indexer_200_responses="{ item }">
       {{ numeral(item.proportion_indexer_200_responses).format('0.00%') }}
     </template>
+    <template v-slot:item.avg_indexer_blocks_behind="{ item }">
+      {{ numeral(item.avg_indexer_blocks_behind).format('0,0') }} blocks
+    </template>
+    <template v-slot:item.max_indexer_blocks_behind="{ item }">
+      {{ numeral(item.max_indexer_blocks_behind).format('0,0') }} blocks
+    </template>
+    <template v-slot:item.num_indexer_200_responses="{ item }">
+      {{ numeral(item.num_indexer_200_responses).format('0,0') }} queries
+    </template>
+
 
   </v-data-table>
 </template>
@@ -75,11 +91,16 @@ const headers = ref([
   { title: 'Query Fees (1d)', key: 'total_query_fees'},
   { title: 'Query Count (1d)', key: 'query_count' },
   //{ title: 'Entities', key: 'upgradeIndexer'},
-  { title: 'Chain ID', key: 'chain_id'},
+  { title: 'Chain', key: 'chain_id'},
   { title: 'Avg Latency', key: 'avg_indexer_latency_ms' },
   { title: 'Avg Query Fee', key: 'avg_query_fee'},
   { title: 'Success Rate', key: 'proportion_indexer_200_responses' },
   { title: 'Deployment ID', key: 'subgraph_deployment_ipfs_hash' },
-
+  { title: 'Avg Blocks Behind', key: 'avg_indexer_blocks_behind'},
+  { title: 'Max Latency', key: 'max_indexer_latency_ms'},
+  { title: 'Max Blocks Behind', key: 'max_indexer_blocks_behind'},
+  { title: 'Successful Queries', key: 'num_indexer_200_responses'},
+  { title: 'Avg Query Fee', key: 'avg_query_fee' },
+  { title: 'Max Query Fee', key: 'max_query_fee' },
 ]);
 </script>
